c1d1ff91dc4f840bb3d1e176ee0f197ce5f18675
maaaasato <154118249+maaaasato@users.noreply.github.com>
2025-08-22 22:47:23 +0900

    Update post_picture_book.py

--- post_picture_book.py
+++ post_picture_book.py
@@ -63,7 +63,7 @@ def fetch_book() -> Dict[str, str]:
         "formatVersion": 2,
         "hits": 20,
         "availability": 1,                 # 在庫あり
-        "booksGenreId": "001004001",
+        "booksGenreId": "001020004",
         "sort": "reviewCount",
         "elements": "title,author,itemCaption,affiliateUrl,itemUrl,reviewAverage,reviewCount",
     }
